30c9fe8f8b7cccb2c9ec82f40fc82de1bd3440c6
Vinod Joshi <vinojos@microsoft.com>
2018-09-11 18:06:33 +0530

    Update uttest.js

--- test/unit/uttest.js
+++ test/unit/uttest.js
@@ -12,68 +12,67 @@ describe('Equality', function() {
     it('should be equal to 10', function() {
         var num = 10;
         expect(num).to.be.eq(10);
-    });
+    })});
     
 describe('Test1', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
     
     describe('Test2', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
         describe('Test3', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
             describe('Test32', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
                 describe('Test35', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
                     describe('Test123', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
                         describe('Test12', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
 describe('Equality12', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
                                 describe('Equality45', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
                                     describe('Equality1', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
                                         describe('Equality452', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
                                             describe('Equality234', function() {
 it('should be equal to 10', function() {
     var num = 10;
     expect(num).to.be.eq(10);
-});
+})});
        
-});
